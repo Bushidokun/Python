07b597e17b45206ad0e8ca3533bdbc44cacc6499
Bushidokun <mrbushido@hotmail.co.uk>
2019-12-19 12:47:08 +0000

    ver 1.1

---
+++ Main/Week 2/8.Multiple Conditions/2 Logical AND Operator/Task.py
@@ -0,0 +1,10 @@
+print ("What did I hear?")
+hearResponse = input()
+
+print ("What did i see?")
+seeResposne = input()
+
+if hearResponse == "grrr" and seeResposne == "two red eyes":
+    print ("There is a scary creature. I should get out of here!")
+else:
+    print ("I am a little scared but I will continue.")
